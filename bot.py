9dbf70edf629a502ae548280fcede321590c9262
mradev-git <94890182+PodcasterID@users.noreply.github.com>
2022-07-07 00:02:15 +0700

    Update bot.py
    Adding more verbose output for which bot being run

--- bot.py
+++ bot.py
@@ -102,7 +102,8 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/randomtalk_me"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/randomtalk_me\n\n"
+            f"[🔥 BOT  @{self.username} AKTIF! 🔥]\n"
         )
 
     async def stop(self, *args):
